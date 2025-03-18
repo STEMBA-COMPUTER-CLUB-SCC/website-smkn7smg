eaac6fc921441cb1b76f2d5d637f75ad98db312e
fasuyaaaPNG <dhavinf.a@gmail.com>
2025-03-18 11:12:58 +0700

    fix(landing): fix margin space on breakpoint 1920px

--- resources/views/components/pages/beranda.blade.php
+++ resources/views/components/pages/beranda.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="ml-auto mr-auto pl-20 max-w-[1780px] pr-23 2xl:pr-30 flex justify-between">
-        <div class="w-max gap-y-28 2xl:gap-y-[200px] flex flex-col">
+        <div class="w-max gap-y-[115px] 2xl:gap-y-[200px] flex flex-col">
             <div>
                 <div class="flex bg-[#F7F7F7] text-[16px] m-0 mb-5 font-manrope border border-[#ebebeb] w-max rounded-[80px] items-center pl-1.5 pt-1.5 pb-1.5 pr-4 gap-x-2">
                     <p class="bg-[#111111] text-[#FFFFFF] font-semibold border border-[#ebebeb] pt-1 pb-1 pl-3 pr-3 rounded-[80px]">STEMBA</p>
