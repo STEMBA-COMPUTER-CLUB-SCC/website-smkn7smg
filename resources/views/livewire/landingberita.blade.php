0dfc0b6eba2117e2e3bb2d9e52f215b275183fc2
fasuyaaaPNG <dhavinf.a@gmail.com>
2025-03-23 00:25:15 +0700

    feat(landing): add animation transition on card prestasi and berita

--- resources/views/livewire/landingberita.blade.php
+++ resources/views/livewire/landingberita.blade.php
@@ -8,7 +8,7 @@
         <p class='font-manrope font-normal text-[20px] text-center text-[#525252] max-w-[531px] ml-auto mr-auto'>Lihat dan baca mengenai seluruh informasi terbaru di SMKN 7 Semarang</p>
     </div>
     <div class='flex gap-x-7 w-max ml-auto mr-auto mb-10'>
-        <div class='group/berita1 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita1 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/berita/band.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'>
                 <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita1:bg-[#FFFFFF] group-hover/berita1:text-[#111111]'>stemba</p>
             </div>
@@ -16,7 +16,7 @@
             <p class='font-manrope text-[20px] font-semibold max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3'>Berberita di Ajang Lomba Band Dies Natalis ke-6 Fakultas Kedokteran Universitas Katolik Soegijapranata Tahun 2025</p>
             <p class='font-manrope text-[14px] font-normal w-[368px] m-0 break-word line-clamp-2'>Final Lomba Band dalam rangka Dies Natalis ke-6 Fakultas Kedokteran Universitas Katolik Soegijapranata (SCU) diadakan pada Jumat, 07 Maret 2025 pukul 14.00 – 19.00 WIB. Acara ini berlangsung di Fakultas Kedokteran SCU, BSB City, Kota Semarang. </p>
         </div>
-        <div class='group/berita2 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita2 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/berita/traspac.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'>
                 <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita2:bg-[#FFFFFF] group-hover/berita2:text-[#D6205D]'>sija</p>
             </div>
@@ -24,7 +24,7 @@
             <p class='font-manrope text-[20px] font-semibold max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3'>Siswa SIJA Berberita di Ajang Traspac Competition Tahun 2025</p>
             <p class='font-manrope text-[14px] font-normal w-[368px] m-0 break-word line-clamp-2'>PT Traspac Makmur Sejahtera adalah salah satu perusahaan yang bergerak di bidang teknologi informasi yang berkonsentrasi di bidang menciptakan budaya digital organisasi dengan peningkatan kompetensi digital SDM, perancangan kembali proses kerja berbasis digital, dan pemanfaatan optimal teknologi informasi Artificial intelligence, Internet of Things, Big Data.</p>
         </div>
-        <div class='group/berita3 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita3 w-[408px] h-[448px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/berita/purna.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'>
                 <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita3:bg-[#FFFFFF] group-hover/berita3:text-[#111111]'>stemba</p>
             </div>
--- resources/views/livewire/landingprestasi.blade.php
+++ resources/views/livewire/landingprestasi.blade.php
@@ -13,21 +13,21 @@
         </div>
     </div>
     <div class='flex gap-x-7 w-max ml-auto mr-auto mb-20'>
-        <div class='group/berita1 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita1 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/prestasi/band.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'></div>
             <p class='font-manrope text-[16px] text-[#525252] font-normal mb-3'>Sabtu, 8 maret 2025</p>
             <p class='font-manrope text-[20px] font-semibold max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3'>Lomba Band Dies Natalis ke-6 Universitas Katolik Soegijapranata Tahun 2025</p>
             <p class='font-manrope text-[14px] font-normal w-[368px] m-0 break-word line-clamp-2 mb-5'>Setelah melewati beberapa tahapan penilaian dari dewan juri, Alhamdulillah 7 Line berhasil meraih prestasi terbaik sebagai Juara I. Selamat kepada Tim Band Stemba Semarang (7 Line) beserta pembimbingnya atas prestasi yang telah diraih. Berikut sebagian cuplikan liputan kegiatan tersebut.</p>
             <p class='text-[#1152D8] font-manrope text-[14px] bg-[#F7F7F7] border border-[#EBEBEB] rounded-[90px] px-5 py-1.5 w-max font-semibold'>PRESTASI</p>
         </div>
-        <div class='group/berita2 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita2 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/prestasi/traspac.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'></div>
             <p class='font-manrope text-[16px] text-[#525252] font-normal mb-3'>Jumat, 01 maret 2025</p>
             <p class='font-manrope text-[20px] font-semibold max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3'>Siswa SIJA Berprestasi di Ajang Traspac Competition Tahun 2025</p>
             <p class='font-manrope text-[14px] font-normal w-[368px] m-0 break-word line-clamp-2 mb-5'>PT Traspac Makmur Sejahtera adalah salah satu perusahaan yang bergerak di bidang teknologi informasi yang berkonsentrasi di bidang menciptakan budaya digital organisasi dengan peningkatan kompetensi digital SDM, perancangan kembali proses kerja berbasis digital, dan pemanfaatan optimal teknologi informasi Artificial intelligence, Internet of Things, Big Data.</p>
             <p class='text-[#1152D8] font-manrope text-[14px] bg-[#F7F7F7] border border-[#EBEBEB] rounded-[90px] px-5 py-1.5 w-max font-semibold'>PRESTASI</p>
         </div>
-        <div class='group/berita3 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
+        <div class='transition-all duration-300 group/berita3 w-[410px] h-[505px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
             <div class='bg-[url(/assets/image/prestasi/cj.png)] h-[240px] w-[368px] rounded-[16px] border bg-cover bg-center relative mb-5'></div>
             <p class='font-manrope text-[16px] text-[#525252] font-normal mb-3'>Selasa, 27 Februari 2025</p>
             <p class='font-manrope text-[20px] font-semibold max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3'>Prestasi Juara 2 di CyberJawara CTF 2024 kategori pelajar</p>
