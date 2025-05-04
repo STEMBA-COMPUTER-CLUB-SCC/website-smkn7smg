976e40624cc6074d3632e7772541b75f16bf9375
yukiiHunter <mdaffaathalla@gmail.com>
2025-05-04 16:44:29 +0700

    feat/admin: adding blog management

--- database/migrations/2025_03_28_074042_create_blogs_table.php
+++ database/migrations/2025_03_28_074042_create_blogs_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('content');
             $table->string('category');
             $table->json('tags')->nullable();
-            $table->string('image')->nullable();
+            $table->string('image');
             $table->timestamps();
         });
     }
