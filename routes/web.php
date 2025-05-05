bcd56813f03f7b4a5a9b91cb0c0851a12397420d
yukiiHunter <mdaffaathalla@gmail.com>
2025-05-05 14:42:01 +0700

    fix (route) : berita and prestasi route adjustment with dynamic content

--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,30 @@
 
 use App\Http\Controllers\KontakController;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Pagejurusan;
 
 Route::get('/', function () {
   return view('/landing/landing');
 });
- 
-use App\Livewire\Pagejurusan;
 
 Route::get('/jurusan/{key}', Pagejurusan::class)->name('jurusan.jurusan');
-
-
-Route::get('/berita', function () {
-  return view('/berita/berita');
-});
-
-Route::get('/berita/detail', function () {
-  return view('/berita/detail');
-});
 
 Route::get('/kontak', function () {
   return view('/kontak/kontak');
 });
-Route::get('/prestasi', function () {
-  return view('/prestasi/prestasi');
+
+Route::get('/berita', function () {
+  return view('berita.berita');
 });
 
-Route::get('/prestasi/detail', function () {
-  return view('/prestasi/detail');
+Route::get('/berita/detail{id}', function ($id) {
+  return view('berita.detail', compact('id'));
+});
+
+Route::get('/prestasi', function () {
+  return view('prestasi.prestasi');
+});
+
+Route::get('/prestasi/detail{id}', function ($id) {
+  return view('prestasi.prestasi', compact('id'));
 });
