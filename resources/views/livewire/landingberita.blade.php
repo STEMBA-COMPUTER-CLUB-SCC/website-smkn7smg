97b537dc1c6326106874524f6f5ff0fb8f128bed
yukiiHunter <mdaffaathalla@gmail.com>
2025-05-06 16:08:10 +0700

    fix (landing) : change card content to dynamic

--- resources/views/livewire/landingberita.blade.php
+++ resources/views/livewire/landingberita.blade.php
@@ -8,30 +8,16 @@
         <p class='deskripsiBerita opacity-0 font-manrope font-normal text-[20px] max-sm:text-[14px] max-sm:w-[243px] text-center text-[#525252] max-w-[531px] ml-auto mr-auto'>Lihat dan baca mengenai seluruh informasi terbaru di SMKN 7 Semarang</p>
     </div>
     <div class='font-manrope flex max-sm:flex-col gap-x-7 max-sm:gap-y-10 w-max ml-auto mr-auto mb-10'>
+        @foreach ($blog as $berita)
         <div class='cardBerita1 opacity-0 transition-all duration-300 group/berita1 w-[408px] max-sm:w-[327px] h-[448px] max-sm:h-[420px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/berita/band.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'>
-                <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita1:bg-[#FFFFFF] group-hover/berita1:text-[#111111]'>stemba</p>
+            <div style="background-image: url({{ asset('storage/' . $berita->image[0]) ?? null }})" class='h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'>
+                <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita1:bg-[#FFFFFF] group-hover/berita1:text-[#111111]'>{{ is_array($berita->tags) ? implode(', ', $berita->tags) : $berita->tags }}</p>
             </div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Sabtu, 8 Maret 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-w-[368px] max-sm:max-w-[295px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Berberita di Ajang Lomba Band Dies Natalis ke-6 Fakultas Kedokteran Universitas Katolik Soegijapranata Tahun 2025</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal w-[368px] max-sm:w-[295px] m-0 break-word line-clamp-2'>Final Lomba Band dalam rangka Dies Natalis ke-6 Fakultas Kedokteran Universitas Katolik Soegijapranata (SCU) diadakan pada Jumat, 07 Maret 2025 pukul 14.00 – 19.00 WIB. Acara ini berlangsung di Fakultas Kedokteran SCU, BSB City, Kota Semarang. </p>
+            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>{{ \Carbon\Carbon::parse($berita->created_at)->translatedFormat('l, j F Y') }}</p>
+            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-w-[368px] max-sm:max-w-[295px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>{{ $berita->title }}</p>
+            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal w-[368px] max-sm:w-[295px] m-0 break-word line-clamp-2'>{{ \Illuminate\Support\Str::limit(strip_tags($berita->content), 120) }}</p>
         </div>
-        <div class='cardBerita2 max-sm:opacity-0 transition-all duration-300 group/berita2 w-[408px] max-sm:w-[327px] h-[448px] max-sm:h-[420px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/berita/traspac.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'>
-                <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita2:bg-[#FFFFFF] group-hover/berita2:text-[#D6205D]'>sija</p>
-            </div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Sabtu, 1 Maret 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-w-[368px] max-sm:max-w-[295px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Siswa SIJA Berberita di Ajang Traspac Competition Tahun 2025</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal w-[368px] max-sm:w-[295px] m-0 break-word line-clamp-2'>PT Traspac Makmur Sejahtera adalah salah satu perusahaan yang bergerak di bidang teknologi informasi yang berkonsentrasi di bidang menciptakan budaya digital organisasi dengan peningkatan kompetensi digital SDM, perancangan kembali proses kerja berbasis digital, dan pemanfaatan optimal teknologi informasi Artificial intelligence, Internet of Things, Big Data.</p>
-        </div>
-        <div class='cardBerita3 opacity-0 transition-all duration-300 group/berita3 w-[408px] max-sm:w-[327px] h-[448px] max-sm:h-[420px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/berita/purna.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'>
-                <p class='absolute right-3 top-3 uppercase py-2 px-5 font-manrope text-[16px] bg-[#F7F7F7] border border-[#EBEBEB] w-max rounded-full font-semibold group-hover/berita3:bg-[#FFFFFF] group-hover/berita3:text-[#111111]'>stemba</p>
-            </div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Jumat, 28 Februari 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-w-[368px] max-sm:max-w-[295px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Selamat Memasuki Purna Tugas Kepada Bapak Drs. Budi Santosa</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal w-[368px] max-sm:w-[295px] m-0 break-word line-clamp-2'>Pensiun atau Purna Tugas adalah seseorang yang sudah tidak bekerja lagi karena usianya sudah lanjut dan harus diberhentikan sesuai dengan peraturan perundang-undangan yang berlaku bagi seorang Pegawai Negeri Sipil.</p>
-        </div>
+        @endforeach
     </div>
     <a href="/berita" class='bg-[#1152D9] w-max font-manrope px-8 py-2.5 border rounded-[42px] text-[16px] text-[#ffffff] font-semibold ml-auto mr-auto block mb-20 max-sm:mb-10'>Selengkapnya</a>
 </section>
--- resources/views/livewire/landingprestasi.blade.php
+++ resources/views/livewire/landingprestasi.blade.php
@@ -13,27 +13,15 @@
         </div>
     </div>
     <div class='font-manrope flex max-sm:flex-col max-sm:gap-y-10 gap-x-7 w-max ml-auto mr-auto mb-20 max-sm:mb-10'>
+        @foreach ($blog as $prestasi)
         <div class='cardPrestasi1 transition-all duration-300 group/berita1 max-sm:w-[327px] w-[410px] h-[505px] max-sm:h-[486px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/prestasi/band.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'></div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Sabtu, 8 maret 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-sm:max-w-[295px] max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Lomba Band Dies Natalis ke-6 Universitas Katolik Soegijapranata Tahun 2025</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal max-sm:w-[295px] w-[368px] m-0 break-word line-clamp-2 mb-5'>Setelah melewati beberapa tahapan penilaian dari dewan juri, Alhamdulillah 7 Line berhasil meraih prestasi terbaik sebagai Juara I. Selamat kepada Tim Band Stemba Semarang (7 Line) beserta pembimbingnya atas prestasi yang telah diraih. Berikut sebagian cuplikan liputan kegiatan tersebut.</p>
+            <div style="background-image: url({{ asset('storage/' . $prestasi->image[0]) ?? null }})" class='h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'></div>
+            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>{{ \Carbon\Carbon::parse($prestasi->created_at)->translatedFormat('l, j F Y') }}</p>
+            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-sm:max-w-[295px] max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>{{ $prestasi->title }}</p>
+            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal max-sm:w-[295px] w-[368px] m-0 break-word line-clamp-2 mb-5'>{{ \Illuminate\Support\Str::limit(strip_tags($prestasi->content), 120) }}</p>
             <p class='text-[#1152D8] font-manrope text-[14px] bg-[#F7F7F7] border border-[#EBEBEB] rounded-[90px] px-5 py-1.5 w-max font-semibold'>PRESTASI</p>
         </div>
-        <div class='cardPrestasi2 opacity-0 transition-all duration-300 group/berita2 max-sm:w-[327px] w-[410px] h-[505px] max-sm:h-[486px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/prestasi/traspac.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'></div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Jumat, 01 maret 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-sm:max-w-[295px] max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Siswa SIJA Berprestasi di Ajang Traspac Competition Tahun 2025</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal max-sm:w-[295px] w-[368px] m-0 break-word line-clamp-2 mb-5'>PT Traspac Makmur Sejahtera adalah salah satu perusahaan yang bergerak di bidang teknologi informasi yang berkonsentrasi di bidang menciptakan budaya digital organisasi dengan peningkatan kompetensi digital SDM, perancangan kembali proses kerja berbasis digital, dan pemanfaatan optimal teknologi informasi Artificial intelligence, Internet of Things, Big Data.</p>
-            <p class='text-[#1152D8] font-manrope text-[14px] bg-[#F7F7F7] border border-[#EBEBEB] rounded-[90px] px-5 py-1.5 w-max font-semibold'>PRESTASI</p>
-        </div>
-        <div class='cardPrestasi3 opacity-0 transition-all duration-300 group/berita3 max-sm:w-[327px] w-[410px] h-[505px] max-sm:h-[486px] rounded-[24px] p-5 bg-[#FFFFFF] border border-[#EBEBEB] hover:border-[#1152D9] hover:border-opacity-30 hover:shadow-[2px_2px_14px_rgba(17,82,217,0.15)]'>
-            <div class='bg-[url(/assets/image/prestasi/cj.png)] h-[240px] w-[368px] max-sm:w-[295px] max-sm:h-[220px] rounded-[16px] border bg-cover bg-center relative mb-5 max-sm:mb-4'></div>
-            <p class='font-manrope text-[16px] max-sm:text-[12px] text-[#525252] font-normal mb-3'>Selasa, 27 Februari 2025</p>
-            <p class='font-manrope text-[20px] max-sm:text-[18px] font-semibold max-sm:max-w-[295px] max-w-[368px] m-0 break-word uppercase line-clamp-2 mb-3 max-sm:mb-2'>Prestasi Juara 2 di CyberJawara CTF 2024 kategori pelajar</p>
-            <p class='font-manrope text-[14px] max-sm:text-[16px] text-[#525252] font-normal max-sm:w-[295px] w-[368px] m-0 break-word line-clamp-2 max-sm:mb-4 mb-5'>Setelah melewati beberapa tahapan penilaian dari dewan juri, Alhamdulillah 7 Line berhasil meraih prestasi terbaik sebagai Juara I. Selamat kepada Tim Band Stemba Semarang (7 Line) beserta pembimbingnya atas prestasi yang telah diraih. Berikut sebagian cuplikan liputan kegiatan tersebut.</p>
-            <p class='text-[#1152D8] font-manrope text-[14px] bg-[#F7F7F7] border border-[#EBEBEB] rounded-[90px] px-5 py-1.5 w-max font-semibold'>PRESTASI</p>
-        </div>
+        @endforeach
     </div>
     <div class="sm:hidden max-sm:mb-10">
         <a href="/prestasi" class='bg-[#1152D9] w-max font-manrope px-8 py-2.5 border rounded-[42px] text-[16px] text-[#ffffff] font-semibold ml-auto mr-auto block'>Selengkapnya</a>
