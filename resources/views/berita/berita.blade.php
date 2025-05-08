5bc51ea76557cdecb47af32ab9aa13382f7f283d
Evandra Raditya <evandraraditya049@gmail.com>
2025-05-08 18:46:19 +0700

    refactor: comment out unused scrollbar CSS in berita and prestasi pages

--- resources/views/berita/berita.blade.php
+++ resources/views/berita/berita.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Berita SMK Negeri 7 Semarang</title>
   @vite('resources/css/app.css')
-  @vite('resources/css/scrollbar.css')
+  {{-- @vite('resources/css/') --}}
   {{--
   <link rel="stylesheet" href="/css/app.css">
   <link rel="stylesheet" href="/css/scrollbar.css"> --}}
--- resources/views/prestasi/prestasi.blade.php
+++ resources/views/prestasi/prestasi.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Prestasi SMK Negeri 7 Semarang</title>
   @vite('resources/css/app.css')
-  @vite('resources/css/scrollbar.css')
+  {{-- @vite('resources/css/scrollbar.css') --}}
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
